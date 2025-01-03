c9096746c40fc2fa526eff1cd0727d666291255a
Pandemonium <jonathancorreaiglesias@gmail.com>
2025-01-03 00:04:11 -0600

    Added the starter code for the project

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,32 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+export default generateMarkdown;
+
+
+// What is your GitHub username?
+// What is your email address?
+// What is your project's name?
+// Please write a short description of your project
+// What kind of license should your project have?
+//     choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+// What command should be run to install dependencies?
+// What command should be run to run tests?
+// What does the user need to know about using the repo?
+// What does the user need to know about contributing to the repo?
